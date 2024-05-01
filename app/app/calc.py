e27e88c9c527ef91f4896498656f51d05d87204a
AndresHinojosa <andrewing.fis@gmail.com>
2024-05-01 16:05:58 -0400

    Django-Postgres connection

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+'''
+Caculator functions
+'''
+
+
+def add(x, y):
+    "Add x and y and return result"
+    return x + y
+
+
+def subtract(x, y):
+    "Subtract x and y return result"
+    return x - y
